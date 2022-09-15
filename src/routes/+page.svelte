bdffa3b8c729133ebfa5872475a927675e1226d3
dewasahu2003 <dewasahu2003@gmail.com>
2022-09-15 12:29:19 +0000

    gitpod configuration and automation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>not able to show another page</h1>
