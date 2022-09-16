b80390850621cbdce0729cdacbf2bb1b913c182b
dewasahu2003 <dewasahu2003@gmail.com>
2022-09-16 21:16:21 +0530

    from vscode

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.css";
+</script>
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <p>hello</p>
+</footer>
+
+<!-- layout.svelte can be placed in the directory you want to theme for and then further things c
+can be customised according to your need like main,footer-->
